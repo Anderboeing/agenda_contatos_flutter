12540b75767b80de6490258b974a046bd23e58d1
Anderson Boeing <anderboeing@gmail.com>
2026-07-09 10:48:33 -0300

    salvando e obtendo contato

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -39,6 +39,27 @@ class ContactHelper {
       },
     );
   }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact?> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(
+      contactTable,
+      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imageColumn],
+      where: "$idColumn = ?",
+      whereArgs: [id],
+    );
+    if (maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
 }
 
 class Contact {
@@ -56,8 +77,8 @@ class Contact {
     image = map[imageColumn];
   }
 
-  Map toMap() {
-    Map<String, dynamic> map = {
+  Map<String, Object?> toMap() {
+    Map<String, Object?> map = {
       nameColumn: name,
       emailColumn: email,
       phoneColumn: phone,
